ba54493ef506bb55b5a44bb7a9ac38d1763be65c
S.Navdeep Singh <navdeep13dps@gmail.com>
2025-01-01 22:15:22 +0530

    Update Ai.js

--- Ai.js
+++ Ai.js
@@ -1,9 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI);
 
 function processText(promt) {
-  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
   const prompt = promt
