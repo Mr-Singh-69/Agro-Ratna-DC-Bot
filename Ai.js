10a5ac4b46abf5334a120c2524eaa1dd5f294bb2
S.Navdeep Singh <navdeep13dps@gmail.com>
2025-01-01 22:17:22 +0530

    Update Ai.js

--- Ai.js
+++ Ai.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI);
 
-function processText(promt) {
+async function processText(promt) {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
   const prompt = promt
